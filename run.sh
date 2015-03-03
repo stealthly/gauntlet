c361e65f8cc51977bd97c4a965284b63f032913c
edgefox <ivanlyutov@gmail.com>
2015-03-03 15:42:52 +0200

    added arguments support for run and validate scripts

--- run.sh
+++ run.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-./gradlew jar
-
 export DATASET_FILE_NAME="dataset"
 export DATASET_SIZE="10000000"
 export DATASET_MIN_LENGTH="1024"
@@ -14,6 +12,54 @@ export KAFKA_CONNECT="localhost:9092"
 export KAFKA_TOPIC="dataset"
 
 export CLIENT_LAUNCH_COMMAND="~/gopath/src/github.com/stealthly/go_kafka_client/mirrormaker/mirrormaker --prefix mirror_ --consumer.config ~/gopath/src/github.com/stealthly/go_kafka_client/mirrormaker/consumer.config --num.streams 2 --producer.config ~/gopath/src/github.com/stealthly/go_kafka_client/mirrormaker/producer.config --whitelist=\"^$KAFKA_TOPIC\""
+
+while [[ $# > 1 ]]
+do
+key="$1"
+
+case $key in
+    --name)
+    DATASET_FILE_NAME="$2"
+    shift
+    ;;
+    --size)
+    DATASET_SIZE="$2"
+    shift
+    ;;
+    --min.length)
+    DATASET_MIN_LENGTH="$2"
+    shift
+    ;;
+    --max.length)
+    DATASET_MAX_LENGTH="$2"
+    shift
+    ;;
+    --producer.config)
+    PRODUCER_CONFIG="$2"
+    shift
+    ;;
+    --zk.connect)
+    ZK_CONNECT="$2"
+    shift
+    ;;
+    --kafka.connect)
+    KAFKA_CONNECT="$2"
+    shift
+    ;;
+    --kafka.topic)
+    KAFKA_TOPIC="$2"
+    shift
+    ;;
+    --client.runner)
+    CLIENT_LAUNCH_COMMAND="$2"
+    shift
+    ;;
+    *)
+            # unknown option
+    ;;
+esac
+shift
+done
 
 eval "java -jar dataset-generator/build/libs/dataset-generator-1.0.jar --filename $DATASET_FILE_NAME --filesize $DATASET_SIZE --min.length $DATASET_MIN_LENGTH --max.length $DATASET_MAX_LENGTH"
 eval "java -jar dataset-producer/build/libs/dataset-producer-1.0.jar --filename $DATASET_FILE_NAME --kafka $KAFKA_CONNECT --topic $KAFKA_TOPIC --producer.config $PRODUCER_CONFIG"
--- validate.sh
+++ validate.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-#export SPARK_PATH="/opt/spark"
-export SPARK_MASTER_URL="spark://Ivans-MacBook-Pro.local:7077"
+export SPARK_PATH="/opt/spark"
+export SPARK_MASTER_URL="spark://localhost:7077"
 
 export CASSANDRA_HOST="localhost"
 export CASSANDRA_USER="cassandra"
@@ -14,5 +14,61 @@ export KAFKA_SOURCE_TOPIC="dataset"
 export KAFKA_DESTINATION_TOPIC="mirror_dataset"
 export KAFKA_FETCH_SIZE="64"
 export KAFKA_NUM_TOPIC_PARTITIONS="1"
+
+while [[ $# > 1 ]]
+do
+key="$1"
+
+case $key in
+    --spark.path)
+    SPARK_PATH="$2"
+    shift
+    ;;
+    --spark.master)
+    SPARK_MASTER_URL="$2"
+    shift
+    ;;
+    --cassandra.host)
+    CASSANDRA_HOST="$2"
+    shift
+    ;;
+    --cassandra.user)
+    CASSANDRA_USER="$2"
+    shift
+    ;;
+    --cassandra.password)
+    CASSANDRA_PASSWORD="$2"
+    shift
+    ;;
+    --zk.connect)
+    ZK_CONNECT="$2"
+    shift
+    ;;
+    --kafka.connect)
+    KAFKA_CONNECT="$2"
+    shift
+    ;;
+    --kafka.source.topic)
+    KAFKA_SOURCE_TOPIC="$2"
+    shift
+    ;;
+    --kafka.destination.topic)
+    KAFKA_DESTINATION_TOPIC="$2"
+    shift
+    ;;
+    --kafka.fetch.size)
+    KAFKA_FETCH_SIZE="$2"
+    shift
+    ;;
+    --kafka.partitions)
+    KAFKA_NUM_TOPIC_PARTITIONS="$2"
+    shift
+    ;;
+    *)
+            # unknown option
+    ;;
+esac
+shift
+done
 
 eval "$SPARK_PATH/bin/spark-submit --conf spark.cassandra.connection.host=$CASSANDRA_HOST --conf spark.cassandra.auth.username=$CASSANDRA_USER --conf spark.cassandra.auth.password=$CASSANDRA_PASSWORD --executor-memory 4G --total-executor-cores 8 --class ly.stealth.shaihulud.reader.Main --master $SPARK_MASTER_URL spark-validator/build/libs/spark-validator-1.0.jar --source $KAFKA_SOURCE_TOPIC --destination $KAFKA_DESTINATION_TOPIC --partitions $KAFKA_NUM_TOPIC_PARTITIONS --zookeeper $ZK_CONNECT --broker.list $KAFKA_CONNECT --kafka.fetch.size $KAFKA_FETCH_SIZE 1> spark-validator.out 2> spark-validator.err"
